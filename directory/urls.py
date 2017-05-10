11988651946a20866a0c3038ef3a07a511acf1f9
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2017-05-10 09:24:15 +0200

    added about page

--- directory/urls.py
+++ directory/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.index_global, name='index_global'),
     url(r'^cs/$', views.index_cs, name='index_cs'),
     url(r'^rfc/$', views.index_rfc, name='index_rfc'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^cs/(?P<cs_name>[a-zA-Z0-9_]+)/$', views.detail_cs, name='detail_cs'),
     url(r'^rfc/(?P<rfc_number>[0-9]+)/$', views.detail_rfc, name='detail_rfc'),
 ]
