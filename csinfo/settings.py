5f75f2b5912997113aafe1c968f52b2406137c0f
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2017-05-15 16:52:54 +0200

    renamed (heroku) production env attribute

--- csinfo/settings.py
+++ csinfo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'HEROKU' in os.environ:
+if 'PRODUCTION' in os.environ:
     DEBUG = False
 else:
     DEBUG = True
