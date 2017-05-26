59c5f9601a8e2102006d318900c10f7d38921ad7
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2017-05-26 18:58:06 +0200

    add word breaks to cipher suite names, generally hide horizontal overflow

---
+++ csinfo/static/js/custom.js
@@ -0,0 +1,11 @@
+// inserts a word break opportunity after each underscore
+// in order to improve the style of long cipher suite names
+function set_breakpoints(){
+    var arr = document.getElementsByClassName('long-string');
+    for(var i = 0; i < arr.length; i++) {
+          arr[i].innerHTML = arr[i].innerHTML.replace(/_/g, '_<wbr/>');
+    }
+}
+
+// execute when page is loaded
+window.onload = set_breakpoints();
