dcd42a3fa99c817b8b1f6c9eedf5d0c6b2b8e6d1
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2017-07-25 08:51:56 +0200

    disable DEBUG mode by default

--- csinfo/settings.py
+++ csinfo/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'PRODUCTION' in os.environ:
-    DEBUG = False
-else:
+if 'DEVELOPMENT' in os.environ:
     DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
