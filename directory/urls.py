bb571a9594bd3f63893e79c35c0c485cc51514b1
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2017-08-22 21:28:43 +0200

    removed news url

--- directory/urls.py
+++ directory/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^cs/$', views.index_cs, name='index_cs'),
     url(r'^rfc/$', views.index_rfc, name='index_rfc'),
-    url(r'^news/$', views.news, name='news'),
     url(r'^about/$', views.about, name='about'),
     url(r'^search/$', views.search, name='search'),
     url(r'^cs/(?P<cs_name>[a-zA-Z0-9_]+)/$', views.detail_cs, name='detail_cs'),
