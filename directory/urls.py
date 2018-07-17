3ab1192e09bd46e688b07df61ec1e87a91b86ef8
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2018-07-17 22:14:38 +0200

    renames static page path to '/page'

--- directory/urls.py
+++ directory/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^cs/$', views.index_cs, name='index_cs'),
     url(r'^rfc/$', views.index_rfc, name='index_rfc'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^static/(?P<sp_name>[a-zA-Z0-9_]+)/$', views.static_page, name='static_page'),
+    url(r'^page/(?P<sp_name>[a-zA-Z0-9_]+)/$', views.static_page, name='static_page'),
     url(r'^cs/(?P<cs_name>[a-zA-Z0-9_]+)/$', views.detail_cs, name='detail_cs'),
     url(r'^rfc/(?P<rfc_number>[0-9]+)/$', views.detail_rfc, name='detail_rfc'),
 ]
