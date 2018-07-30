39da03c7a931cbcc4eac1a09c36d99f804128cb1
Hans Christian Rudolph <hcrudolph@posteo.de>
2018-07-30 22:00:04 +0000

    adds deployment script for convenience

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3.6 manage.py migrate
+python3.6 manage.py loaddata directory/fixtures/*
+python3.6 manage.py scrapeiana
+python3.6 manage.py filltlsversion
+python3.6 manage.py collectstatic --noinput
