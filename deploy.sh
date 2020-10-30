98472c6e00acb5b6567f548705e7a5513c02a9a8
Hans Christian Rudolph <hans@hcrudolph.com>
2020-10-30 14:12:59 +0900

    update deploy script to generic python3

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3.7 manage.py migrate
-python3.7 manage.py loaddata directory/fixtures/*
-python3.7 manage.py scrapeiana
-python3.7 manage.py filltlsversion
-python3.7 manage.py updatesecurity
-python3.7 manage.py compilescss
-python3.7 manage.py collectstatic -c --noinput
-python3.7 manage.py compress
-python3.7 manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py loaddata directory/fixtures/*
+python3 manage.py scrapeiana
+python3 manage.py filltlsversion
+python3 manage.py updatesecurity
+python3 manage.py compilescss
+python3 manage.py collectstatic -c --noinput
+python3 manage.py compress
+python3 manage.py collectstatic --noinput
