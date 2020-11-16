3b5374c4a33d05656e72ba23bed87659c7eb18b6
Hans Christian Rudolph <hans@hcrudolph.com>
2020-11-16 11:41:39 +0900

    fixes ccm parsing to include ccm-only ciphers

--- directory/signals.py
+++ directory/signals.py
@@ -105,8 +105,8 @@ def complete_cs_instance(sender, instance, *args, **kwargs):
         (enc,_,hsh) = rst.rpartition(" ")
 
         # split enc again if we only got a number for hsh
-        # specifically needed for 'CCM 8' hash algorithm
-        if re.match(r'\d+', hsh.strip()):
+        # specifically needed for CCM/CCM8 ciphers
+        if re.match(r'\d+', hsh.strip()) or re.match(r'CCM\Z', hsh.strip()):
             enc += " " + hsh
             hsh = "SHA256"
 
