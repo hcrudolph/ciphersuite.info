c6d252523fb788418cc3dcd7898a42f470d050b0
Hans Christian Rudolph <devel@hcrudolph.com>
2022-08-13 18:45:47 +0900

    updates dependencies; bump Django to v4.1

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 # django imports
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
--- directory/models.py
+++ directory/models.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.db.models.fields.related import ManyToManyField
 from django.db import models
